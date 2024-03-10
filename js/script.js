25d639932df2253759f511cfff0dcdecea54d3d1
Ali Koolivand <alisfp43@gmail.com>
2024-03-10 12:42:26 +0330

    changing the variable name

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 // api key = b7cfffa5610dac46cc196b4a4a8de699
 const searchInp = document.querySelector(".search-inp");
 const searchBtn = document.querySelector(".search-icon");
-const weatherIcon = document.querySelector(".weather-icon");
+const weatherImg = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
 const cityName = document.querySelector(".city-name");
 const humidityPercentage = document.querySelector(".humidity-percentage");
 const windSpeed = document.querySelector(".wind-speed");
 // FUNCTIONS
+
 // Fetching data and request to server with async await
 const fetcher = async function (cityName) {
   // Using try catch block
@@ -18,6 +19,8 @@ const fetcher = async function (cityName) {
     console.log(deliver);
     if (!deliver.ok)
       throw new Error("Cant find your city, Please add another city");
+
+    // Returning the data
     return await deliver.json();
   } catch (error) {
     // Error handling
@@ -29,14 +32,18 @@ const fetcher = async function (cityName) {
 searchBtn.addEventListener("click", async function () {
   // Using guard clause
   if (!searchInp.value) return;
+
   const data = await fetcher(searchInp.value);
   searchInp.value = "";
   // check for data validity
   if (!data) return;
+
   // Inserting data text into the page
   cityName.textContent = data.name;
   humidityPercentage.textContent = `${data.main.humidity}%`;
   temperature.textContent = `${Math.round(data.main.temp)} °C`;
   windSpeed.textContent = `${data.wind.speed} km/h`;
-  weatherIcon.src = `svg/${data.weather[0].main}.svg`;
+  weatherImg.src = `svg/${data.weather[0].main}.svg`;
 });
+
+// @js-challenges
